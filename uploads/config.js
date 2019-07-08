582c42e66d9974a979ce9c6df34c5026b330a345
alezzigo <aleximong@gmail.com>
2019-07-08 14:05:22 +0700

    Update config.js

--- uploads/config.js
+++ uploads/config.js
@@ -1,1 +1,3 @@
-window.configDomain ="http://localhost:3000";
+window.configDomain = "http://localhost:3000";
+window['configDomain'] = "http://localhost:3000";
+var configDomain = "http://localhost:3000";
