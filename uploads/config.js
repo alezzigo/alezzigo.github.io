2541bd5dfbd3cc73dd3b229e29ba3564804644a3
alezzigo <aleximong@gmail.com>
2019-07-08 19:21:40 +0700

    Update config.js

--- uploads/config.js
+++ uploads/config.js
@@ -1,3 +1,3 @@
-window.configDomain = "http://18.191.40.176";
-window['configDomain'] = "http://18.191.40.176";
-var configDomain = "http://18.191.40.176";
+window.configDomain = "http://vikayo.com";
+window['configDomain'] = "http://vikayo.com";
+var configDomain = "http://vikayo.com";
